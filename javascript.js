102578ba4a18e20886d54fd81013f4abd1356761
최선희 <choiyoface@gmail.com>
2023-07-02 18:59:41 +0200

    add function for userChoice

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
-let arr = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice(){
-    console.log(arr[Math.floor(Math.random() * arr.length)])
-   
+const getUserChoice=userInput=>{
+    userInput=userInput.toLowerCase();
+    if (userInput === 'rock'|| userInput==='scissors' || userInput==='paper'){
+        return userInput
+    }else{
+        console.log("Error! please type: rock, paper or scissors");
+    }
+}
+console.log(getUserChoice('papepp'));
 
+const arr = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    console.log(arr[Math.floor(Math.random() * arr.length)]);
 }
 
-getComputerChoice()
