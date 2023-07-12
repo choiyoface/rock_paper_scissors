ca335dcec5b82fcb687774a8fb9cf7639dfbcfba
최선희 <choiyoface@gmail.com>
2023-07-12 23:52:20 +0200

    add final result and reset score

--- javascript.js
+++ javascript.js
@@ -4,14 +4,15 @@ const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 const choiceBtn = document.querySelectorAll(".choiceBtn");
-
+const finalScore = document.querySelector("#final-score");
 let playerSelection
 let computerSelection
 let result;
 let pScore = 0;
 let cScore = 0;
+let finalWinner;
 
-
+//when Btn is clicked
 choiceBtn.forEach(button => button.addEventListener("click", ()=> {
     playerSelection = button.textContent;
     getComputerChoice();
@@ -22,30 +23,35 @@ choiceBtn.forEach(button => button.addEventListener("click", ()=> {
 }))
 
 
-const getComputerChoice=()=>{
-    const randomNumber = Math.floor(Math.random()*3);
-    switch (randomNumber){
-        case 0:
-            computerSelection = 'Rock';
-            break;
-        case 1:
-            computerSelection ='Paper';
-            break;
-        case 2:
-            computerSelection = 'Scissors';
-            break;
-    }
-}
 
-
+//add and reset score
 const updateScore = () => {
     const playerScore = document.querySelector(".player-score p");
     const computerScore = document.querySelector(".computer-score p");
     playerScore.textContent = pScore;
     computerScore.textContent = cScore;
 
+    if (pScore === 5 || cScore === 5){
+        resetSCore();
+        finalResult();
+        return 
+    }
+
+}
+function finalResult(){
+    if (pScore > cScore){
+        finalScore.textContent = `Final Winner is You`
+    }else{
+        finalScore.textContent = `Final Winner is Computer`
+    }
 }
 
+function resetSCore(){
+    pScore = 0;
+    cScore = 0;
+}
+
+//determine which is the winner
 function determineWinner(){
     if (playerSelection===computerSelection){
         return "It is a draw!";
@@ -89,4 +95,18 @@ function determineWinner(){
      
 }
 
-
+//random choice for computer
+const getComputerChoice=()=>{
+    const randomNumber = Math.floor(Math.random()*3);
+    switch (randomNumber){
+        case 0:
+            computerSelection = 'Rock';
+            break;
+        case 1:
+            computerSelection ='Paper';
+            break;
+        case 2:
+            computerSelection = 'Scissors';
+            break;
+    }
+}
